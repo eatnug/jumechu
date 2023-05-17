e466357bb27a025f00522f12fa38edae0812d82d
eatnug <wlq4568@gmail.com>
2023-05-17 18:51:52 +0900

    add: priority logic

--- src/main/kotlin/com/example/jumechu/receommendation/RecommendationService.kt
+++ src/main/kotlin/com/example/jumechu/receommendation/RecommendationService.kt
@@ -4,6 +4,7 @@ import com.example.jumechu.menu.Menu
 import com.example.jumechu.menu.MenuRepository
 import com.example.jumechu.user.UserRepository
 import org.springframework.stereotype.Service
+import java.util.Collections
 
 @Service
 class RecommendationService(private val userRepository: UserRepository, private val menuRepository: MenuRepository) {
@@ -13,17 +14,21 @@ class RecommendationService(private val userRepository: UserRepository, private 
             throw IllegalStateException("유저 정보를 찾을 수 없습니다.")
         }
         val users = queriedUsers.toList()
-
+        val liked = users.map { it.likedMenus }.flatten()
         val disliked = users.map { it.dislikedMenus }.flatten()
 
         val menus = menuRepository.findAll().filter {
             it !in disliked
-        }
+        }.map {
+            MenuPreference(menu = it, likedCount = Collections.frequency(liked, it))
+        }.shuffled()
 
         if (menus.isEmpty()) {
             throw IllegalStateException("모두가 싫어하지 않는 메뉴를 찾을 수 없습니다.")
         }
-        
-        return menus[0]
+
+        return menus.sortedByDescending { it.likedCount }[0].menu
     }
 }
+
+data class MenuPreference(val menu: Menu, val likedCount: Int)
